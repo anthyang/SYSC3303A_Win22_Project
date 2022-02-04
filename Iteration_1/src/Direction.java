f18af88313164699afba8a0abd8a57e63c3edd98
Nicolas Tuttle <72220827+nicolastuttle@users.noreply.github.com>
2022-02-04 13:47:31 -0500

    Implement basic floor subsystem (#2)
    Scheduler also refactored to accomodate merge

--- Iteration_1/src/Direction.java
+++ Iteration_1/src/Direction.java
@@ -1,3 +1,31 @@
+import java.util.Locale;
+
+/**
+ * Describes the direction an elevator should move in the system
+ */
 public enum Direction {
-    UP, DOWN, NOT_MOVING
+    UP("up"), DOWN("down"), NOT_MOVING("not moving");
+
+    /** A readable string for the enum value */
+    private final String readable;
+
+    Direction(String readable) {
+        this.readable = readable;
+    }
+
+    /**
+     * Get an enum value with the given search value, case-insensitive
+     * @param search The desired enum value
+     * @return The enum matching the search term
+     * @throws IllegalArgumentException when no valid enum was found
+     */
+    public static Direction get(String search) {
+        for (Direction direction : Direction.values()) {
+            if (direction.readable.equals(search.toLowerCase(Locale.ROOT))) {
+                return direction;
+            }
+        }
+
+        throw new IllegalArgumentException("Specified enum was not found");
+    }
 }
--- Iteration_1/src/Floor.java
+++ Iteration_1/src/Floor.java
@@ -1,15 +1,65 @@
-public class Floor implements Runnable {
-    private int floorNum;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-    public Floor() {
+/**
+ * This class models a floor controller in the elevator system
+ */
+public class Floor implements Runnable {
+    /** The input file for the program's requests */
+    public static final String INPUT_FILE = "Iteration_1/src/input";
+
+    /** Constants to reference different parts of the input file */
+    private static final int TIME = 0;
+    private static final int SOURCE_FLOOR = 1;
+    private static final int DIRECTION_BUTTON = 2;
+    private static final int DEST_FLOOR = 3;
+
+    private Scheduler scheduler;
+
+    /**
+     * Create a new floor subsystem controller
+     * @param scheduler The scheduler that will handle requests
+     */
+    public Floor(Scheduler scheduler) {
+        this.scheduler = scheduler;
     }
 
-    public void requestElevator(int floorButtonNum, int elevDoorNum) {
-
+    /**
+     * Send an elevator request to the scheduler
+     * @param sourceFloor Floor the new passenger is on
+     * @param destFloor Passenger's destination floor
+     * @param direction Direction pressed on the floor controller
+     */
+    public void requestElevator(int sourceFloor, int destFloor, Direction direction) {
+        this.scheduler.addToServiceQueue(sourceFloor, destFloor, direction);
     }
 
     @Override
     public void run() {
+        this.readInputFile();
+    }
 
+    /**
+     * Parse the input file and queue requests for elevators
+     */
+    private void readInputFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader(INPUT_FILE))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // Ignore comment lines
+                if (!line.startsWith("#")) {
+                    String[] instructions = line.split(" ");
+
+                    Direction chosenDirection = Direction.get(instructions[DIRECTION_BUTTON]);
+                    int sourceFloor = Integer.parseInt(instructions[SOURCE_FLOOR]);
+                    int destFloor = Integer.parseInt(instructions[DEST_FLOOR]);
+
+                    this.requestElevator(sourceFloor, destFloor, chosenDirection);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
