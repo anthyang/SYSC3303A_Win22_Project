8444ad0edeb70f2bb64a85e56352dbc16d649306
Nicolas Tuttle <nicolastuttle@cmail.carleton.ca>
2022-02-19 21:04:56 -0500

    Change constants to use Config class

--- src/Config.java
+++ src/Config.java
@@ -1,16 +1,12 @@
 /**
- * 
- */
-
-/**
  * @author Phuc La
  *This is the configuration class that contain the constant that will be use in the program
  */
 public class Config {
-	public final int DOOR_MOVEMENT = 4590;
-	public final int ELEVATOR_MOVEMENT = 2832;
-	public final int NUMBER_OF_ELEVATORS = 1;
-	public final int NUMBER_OF_FLOORS = 7;
+	public static final int DOOR_MOVEMENT = 4590;
+	public static final int ELEVATOR_MOVEMENT = 2832;
+	public static final int NUMBER_OF_ELEVATORS = 1;
+	public static final int NUMBER_OF_FLOORS = 7;
 	public static final int TIME = 0;
     public static final int SOURCE_FLOOR = 1;
     public static final int DIRECTION_BUTTON = 2;
