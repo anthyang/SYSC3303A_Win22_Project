5dcda315b5ac1cdf908bd79d3d5b1048f4214db5
Nicolas Tuttle <nicolastuttle@cmail.carleton.ca>
2022-03-09 20:49:07 -0500

    Implement majority of Elevator changes for iter 3

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,13 +1,7 @@
-import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
-import java.net.UnknownHostException;
-import java.sql.Timestamp;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.*;
 
 /**
  * Elevator subsystem class receives information packets from the scheduler to control the elevator motors and to open the doors.
@@ -15,82 +9,103 @@ import java.util.*;
  * @author Gilles Myny
  * @id 101145477
  */
-public class Elevator implements Runnable {
+public class Elevator extends Host implements Runnable {
     private int elevDoorNum;
-    private Boolean floorLamps[];
-    private Scheduler scheduler;
+    private boolean[] floorLamps;
     private int currentFloor;
-    private Set<Integer> floorsToVisit;
-	private List<Request> req;
 	private Direction direction;
+	private boolean doorsOpen;
         
     // add direction lamps to donate arrival and direction of an elevator at a floor
     private int dirLamps;
     
     // add datagram for notifying scheduler
-   // private DatagramPacket sendPacket, receivePacket;
-   // private DatagramSocket sendSocket;
-   // private int port = 5000;
+	private DatagramSocket sendSocket;
 
     /**
      * The Elevator constructor initializes all necessary variables.
      * @param id represents an Integer of the elevator's identification number.
-     * @param sch represents a Scheduler object.
      * @param floorCount represents an Integer of the maximum amount of floors in the building.
      */
-    public Elevator(int id, Scheduler sch, int floorCount) {
+    public Elevator(int id, int floorCount) {
+		super("Elevator " + id);
     	this.elevDoorNum = id;
     	this.currentFloor = 1;
-    	this.scheduler = sch;
-		sch.registerElevator(this);
-    	floorLamps = new Boolean[floorCount];
-    	floorsToVisit = new HashSet<>();
-    	// -1 for down, 0 - not moving, 1 for up:
-    	dirLamps = 0;     	
+    	this.floorLamps = new boolean[floorCount];
+    	this.dirLamps = 0; // -1 for down, 0 - not moving, 1 for up:
+		this.doorsOpen = true;
     	
-    	// for iteration #3
-      /*  try {
+        try {
         	sendSocket = new DatagramSocket();
          } catch (SocketException se) {
             se.printStackTrace();
-            System.exit(1);
-         } */
+         }
 
     }
 
     /**
-     * Runs the moveElevator() method endlessly.
+     * Picks up and drops off passengers endlessly
      */
     public void run() {
-		openDoor();
-		req = this.scheduler.updateQueue(this, this.currentFloor);
-		while (req != null) {
-    		moveElevator();
-			req = this.scheduler.updateQueue(this, this.currentFloor);
-    	}
+		while (true) {
+			if (!this.doorsOpen) {
+				// Ensure passengers can load/unload
+				this.openDoor();
+			}
+
+			// Ask scheduler for next direction of travel
+			this.direction = this.serveNewRequest();
+			if (this.direction == Direction.NOT_MOVING) {
+				// Notify the scheduler that the passengers have been picked up
+				this.notifyArrival(false);
+			} else {
+				this.closeDoor();
+				do {
+					// Move elevator up/down until the scheduler tells it to stop and open its doors
+					this.simMovement();
+				} while (!this.notifyArrival(true));
+			}
+		}
     }
+
+	private Direction serveNewRequest() {
+		// Response should be an array of one byte with -1, 0, or 1
+		DatagramPacket response = this.receive(this.sendSocket);
+
+		byte[] responseData = response.getData();
+		if (response.getLength() != 1 || responseData[0] < -1 || responseData[0] > 1) {
+			throw new RuntimeException("Invalid response from Scheduler");
+		}
+
+		switch (responseData[0]) {
+			case -1:
+				return Direction.DOWN;
+			case 0:
+				return Direction.NOT_MOVING;
+			default:
+				return Direction.UP;
+		}
+	}
     
     /**
      * Simulates the movement of the elevator between each floor and updates the currentFloor variable.
-     * @param dir represents Direction enumerator of the elevator.
      */
-    public void simMovement(Direction dir) {
-    	System.out.println(getTimestamp() + " Elevator " + this.elevDoorNum + " is moving.");
+    public void simMovement() {
+		this.log("Moving.");
     	try {
     		Thread.sleep(Config.ELEVATOR_MOVEMENT);
-    	} catch(InterruptedException e) {
-    		System.err.println(e);
+    	} catch (InterruptedException e) {
+    		e.printStackTrace();
     	}
-    	if(dir == Direction.UP) {
+
+    	if (this.direction == Direction.UP) {
 			currentFloor++;
-			System.out.println(getTimestamp() + " Elevator " + this.elevDoorNum + " is at floor "+this.currentFloor);
 			dirLamps = 1;
 		} else {
 			currentFloor--;
-			System.out.println(getTimestamp() + " Elevator " + this.elevDoorNum + " is at floor "+this.currentFloor);
 			dirLamps = -1;
-		}    	
-    	
+		}
+		this.log("At floor " + this.currentFloor);
     }
     
     /**
@@ -98,124 +113,66 @@ public class Elevator implements Runnable {
      * is calculated to be half the average unloading/loading time.
      */
     private void openDoor() {    	
-    	if (dirLamps != 0) {
-    		// notify scheduler if an elevator arrived at a floor;
-    		notifyArrival();
-    		// turn directon lamp off once arrival is completed.
-    		dirLamps = 0;
-    	}
-    	System.out.println(getTimestamp() + " Elevator " + this.elevDoorNum + " is opening doors at floor " + this.currentFloor);
+		this.log("Opening doors at floor " + this.currentFloor);
     	try {
     		Thread.sleep(Config.DOOR_MOVEMENT);
-    	} catch(InterruptedException e) {
-    		System.err.println(e);
+    	} catch (InterruptedException e) {
+    		e.printStackTrace();
     	}
     }
 
-	/**
-	 * Get a string representation of the current time
-	 * @return a timestamp
-	 */
-	private String getTimestamp() {
-		return Timestamp.from(ZonedDateTime.now().toInstant().truncatedTo(ChronoUnit.SECONDS)).toString();
-	}
+
 
     /**
      * Simulates the doors of the elevator closing given the DOOR_MOVEMENT constant which
      * is calculated to be half the average unloading/loading time.
      */
     private void closeDoor() {
-    	System.out.println(getTimestamp() + " Elevator " + this.elevDoorNum + " is closing doors at floor " + this.currentFloor);
+    	this.log("Closing doors at floor " + this.currentFloor);
 		try {
     		Thread.sleep(Config.DOOR_MOVEMENT);
-    	} catch(InterruptedException e) {
-    		System.err.println(e);
+    	} catch (InterruptedException e) {
+    		e.printStackTrace();
     	}
     }
-
-    /**
-     * Handles the elevator's functionality, ranging from commanding the doors to open
-     * and close, to moving the elevator to specific floors given information packets
-     * called Request object's from the scheduler.
-     */
-    public void moveElevator() {
-    	closeDoor();
-
-		int sourceFloor = 1;
-		direction = Direction.NOT_MOVING;
-		for (Request request : req) {
-			sourceFloor = request.getSourceFloor();
-			floorsToVisit.add(request.getDestFloor());
-			floorLamps[request.getDestFloor() - 1] = true;
-			direction = request.getDirection();			
-		}
-    	
-    	while(!(currentFloor == sourceFloor)) {    		
-			simMovement((sourceFloor > currentFloor) ? Direction.UP : Direction.DOWN);
-		}
-    	
-    	while(!floorsToVisit.isEmpty()) {
-    		simMovement(direction);
-			req = scheduler.updateQueue(this, currentFloor);
-    		for(Request r : req) {
-    			floorsToVisit.add(r.getDestFloor());
-    			floorLamps[r.getDestFloor() - 1] = true;
-    		}
-    		if((reqAtCurrFloor()) || (floorsToVisit.contains(currentFloor))) {
-    			openDoor();
-    			floorsToVisit.remove(currentFloor);
-    			floorLamps[currentFloor - 1] = false;
-				if (!floorsToVisit.isEmpty()) {
-					closeDoor();
-				}
-    		}
-    	}
-    }
-
-	/**
-	 * Check whether there is a pending request at the current floor
-	 * @return true if a request is pending, false otherwise
-	 */
-	private boolean reqAtCurrFloor() {
-		return req.stream().anyMatch(request -> request.getSourceFloor() == currentFloor);
-	}
 
 	/**
 	 * Notify the scheduler of arrival at a floor
+	 * @return true if the elevator is stopping, false otherwise
 	 */
-	public void notifyArrival() {
-    	this.scheduler.serviceComplete(this, this.currentFloor);
-    	
-    	/* for iteration #3
-        String s = "arrived at floor " + currentFloor;
-        System.out.println("Elevator: notifying scheduler floor arrival");
+	public boolean notifyArrival(boolean wasMoving) {
+		ElevatorReport report = new ElevatorReport(this.elevDoorNum, this.direction, this.currentFloor);
+        this.log("Notifying scheduler of arrival at floor " + this.currentFloor);
 
-        byte msg[] = s.getBytes();
+        byte[] msg = report.serialize();
 
-       try {
-           sendPacket = new DatagramPacket(msg, msg.length,
-                                           InetAddress.getLocalHost(), port);
-        } catch (UnknownHostException e) {
-           e.printStackTrace();
-           System.exit(1);
-        }
+		this.log("Notifying scheduler at " + InetAddress.getLoopbackAddress() + ":" + Scheduler.ELEVATOR_UPDATE_PORT);
+		this.log("Data: " + new String(msg));
+		DatagramPacket response = this.rpcCall(
+				this.sendSocket,
+				msg,
+				InetAddress.getLoopbackAddress(),
+				Scheduler.ELEVATOR_UPDATE_PORT
+		);
 
-        System.out.println("elevator: notifying:");
-        System.out.println("scheduler: " + sendPacket.getAddress());
-        System.out.println("scheduler port: " + sendPacket.getPort());
-        System.out.print("with data: ");
-        System.out.println(new String(sendPacket.getData()));
+		byte[] responseData = response.getData();
+		if (response.getLength() != 1 || (responseData[0] != 0 && responseData[0] != 1)) {
+			throw new RuntimeException("Invalid response from Scheduler");
+		}
 
-        try {
-           sendSocket.send(sendPacket);
-        } catch (IOException e) {
-           e.printStackTrace();
-           System.exit(1);
-        }
+		if (wasMoving) {
+			// Log messages to say we're stopping
+			if (responseData[0] == 1) {
+				this.log("Stopping at floor " + this.currentFloor);
+				return true;
+			} else {
+				this.log("Nothing to service, continuing.");
+				return false;
+			}
+		}
 
-        System.out.println("Elevator: notified Scheduler.\n"); */
-  	
-    }
+		return true;
+	}
 
 	/**
 	 * Get the elevator's direction
@@ -229,14 +186,8 @@ public class Elevator implements Runnable {
 	 * Get the elevator's current floor
 	 * @return the current floor
 	 */
-	public int getCurrentFloor() {return currentFloor;}
-
-	/**
-	 * Get the set of floors to visit
-	 * @return the floors to visit
-	 */
-	public Set<Integer> getFloorToVisit() {
-		return floorsToVisit;
+	public int getCurrentFloor() {
+		return currentFloor;
 	}
 
 	public static void main(String[] args) {
